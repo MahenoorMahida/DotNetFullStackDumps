b50d47d7931eecc881af06789f2b855966b0a372
Mahenoor Mahida <34581158+MahenoorMahida@users.noreply.github.com>
2023-09-11 22:44:16 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DotNetFullStackDumps
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello GitHub");
+      Console.WriteLine("Our 1st C# app");
+      
+    }
+  }
+}
